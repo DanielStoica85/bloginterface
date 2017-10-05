242a792c0cf927db312f1c7718e3132a3c989f96
Daniel <dansto13@yahoo.com>
2017-10-05 22:46:09 +0000

    Add two header options.

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100vw";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+} 
