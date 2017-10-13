1b5df444c40734c4aadacdec331a5fa5f18a1854
Daniel <dansto13@yahoo.com>
2017-10-13 09:57:41 +0000

    Add postSchema methods.

--- blogapp.js
+++ blogapp.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
 // ### DB CONNECTION ###
 
 // Connect to DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blogapp_v1", {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blogapp_v1", { useMongoClient: true });
 let db = mongoose.connection;
 
 // Check DB connection
@@ -47,51 +47,52 @@ app.set('view engine', 'ejs');
 
 // Home Route
 app.get('/', (req, res) => {
-  Post.find({}, (err, allPosts) => {
-      if (err) {
-          console.log(err);
-      }
-      else {
-          res.render('frontend/home', {posts: allPosts, title: 'Tutorials Blog - Home'});
-      }
-  });
+    Post.getAll((allPosts) => {
+        res.render('frontend/home', { posts: allPosts, title: 'Tutorials Blog - Home' });
+    });
 });
 
 // Admin Route
 app.get('/admin', (req, res) => {
-    res.render('admin/admin', {title: 'Blog Admin'});
+    Post.find({}, (err, allPosts) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(allPosts.length);
+            res.render('admin/admin', { title: 'Blog Admin', posts: allPosts });
+        }
+    });
 });
 
 // Show form to add new Post
 app.get('/posts/new', (req, res) => {
     res.locals.containsEditor = true;
-    res.render('admin/newPost', {title: 'Add Post'});
+    Post.getAll((allPosts) => {
+        res.render('admin/newPost', { title: 'Add Post', posts: allPosts });
+    });
 });
 
 // Add new Post (after submitting form) route
 app.post('/', (req, res) => {
-    console.log(req.body);
-    console.log(req.body.category);
     let title = req.body.title;
     let image = req.body.image;
     let content = req.body.content;
     let author = req.body.author;
     let category = req.body.category;
-    let newPost = {title: title, image: image, content: content, author: author, category: category};
-    Post.create(newPost, (err, addedPost) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.redirect('/');
-        }
+    let newPost = { title: title, image: image, content: content, author: author, category: category };
+    console.log(Post);
+    Post.addPost(newPost, () => {
+        res.redirect('/');
     });
 });
 
 // Show form to add new Post
 app.get('/pages/new', (req, res) => {
     res.locals.containsEditor = true;
-    res.render('admin/newPage', {title: 'Add Post'});
+    Post.getAll((allPosts) => {
+        res.render('admin/newPage', { title: 'Add Post' });
+    });
 });
 
 // Show entire blog post
@@ -101,37 +102,36 @@ app.get('/post/:id', (req, res) => {
             console.log(err);
         }
         else {
-            res.render('frontend/showpost', {post: foundPost, title: 'Demo Postshow'});
-            console.log(foundPost);
+            res.render('frontend/showpost', { post: foundPost, title: 'Demo Postshow' });
         }
     });
 });
 
 // Show list of pages
 app.get('/admin/pages', (req, res) => {
-    res.render('admin/pages', {title: 'Pages', page: 'pages'});
+    res.render('admin/pages', { title: 'Pages', page: 'pages' });
 });
 
 // Show list of pages
 app.get('/admin/posts', (req, res) => {
-    res.render('admin/posts', {title: 'Posts'});
+    res.render('admin/posts', { title: 'Posts' });
 });
 
 // Show list of users
 app.get('/admin/users', (req, res) => {
-    res.render('admin/users', {title: 'Users'});
+    res.render('admin/users', { title: 'Users' });
 });
 
 // Show login form
 app.get('/admin/login', (req, res) => {
-    res.render('admin/login', {title: 'Login'});
+    res.render('admin/login', { title: 'Login' });
 });
 
 // Add user route
 app.get('/users/new', (req, res) => {
-    res.render('admin/newUser', {title: 'Add User'});
+    res.render('admin/newUser', { title: 'Add User' });
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
-  console.log('Server started on port ' + process.env.PORT + '.');
+    console.log('Server started on port ' + process.env.PORT + '.');
 });
--- models/post.js
+++ models/post.js
@@ -11,4 +11,32 @@ let postSchema = new Schema({
     author: {type: String, default: 'Daniel'}
 });
 
+postSchema.statics.addPost = function (newPost, callback) {
+    let Post = this;
+    Post.create(newPost, (err, addedPost) => {
+        if (err) {
+            console.log('DB error!');
+            console.log(err);
+        }
+        else {
+            console.log(addedPost._id);
+            callback();
+        }
+    });
+};
+
+postSchema.statics.getAll = function (callback) {
+    let Post = this;
+    Post.find({}, (err, foundPosts) => {
+        if (err) {
+            console.log('DB error!');
+            console.log(err);
+        }
+        else {
+            console.log(foundPosts.length);
+            callback(foundPosts);
+        }
+    });
+};
+
 module.exports = mongoose.model("Post", postSchema);
